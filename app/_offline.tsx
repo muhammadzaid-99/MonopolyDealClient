b5ca0b90214711faeb4b5a498f52e24c0fae92ef
Muhammad Zaid <muhammadzaid00912@gmail.com>
2025-07-04 04:15:18 +0500

    offline

---
+++ app/_offline.tsx
@@ -0,0 +1,7 @@
+export default function Offline() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-xl font-bold">You are offline</h1>
+    </div>
+  );
+}
--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,8 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
-  disable: false,
+  register: true,
+  skipWaiting: true,
 });
 
 
